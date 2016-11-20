a5cc42729e33fa7f4fd29dc98cc38d862e2273e8
yonaides <yonaides@hotmail.com>
2016-11-20 18:12:07 -0400

    Firts Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Formularios en AngularJS';
+  form: FormGroup;
+
+  constructor( public fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre:'',
+      apellido:'',
+      cedula:['',Validators.required],
+      email:'',
+      notificacion:'No',
+      telefonosDT:this.fb.group({
+        
+
+      })
+        
+    });
+  }
+
+public setValores():void{
+console.log('formularios', this.form.controls);
+
+  this.form.controls['nombre'].setValue('yonaides');
+  this.form.controls['apellido'].setValue('tavares');
+  this.form.controls['cedula'].setValue('');
+  this.form.controls['email'].setValue('yonaides@hotmail.com');
+  this.form.controls['notificacion'].setValue('');
+
+
+}
+
+public reset():void{
+  this.form.reset();
+}
+
+
+public setAddTelefonos():void{
+
+
+
+
+}
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
